883fab106f063348cad8f3b30a43479245a786b2
Vivek Sanghvi <vivek.sanghvi@stonybrook.edu>
2019-05-25 16:26:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Hello, Neil deGrasse Tyson!"
 
-var greetSpecialGuest = "Neil deGrasse Tyson" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest;
 
 var topic = "space";
 
